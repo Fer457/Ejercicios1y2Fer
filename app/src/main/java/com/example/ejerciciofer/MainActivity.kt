c0e9a16868b953c7efed25dce9b2e6d139b6134b
Fer457 <fernandonevothernandez@gmail.com>
2022-11-09 23:17:54 +0100

    Ejercicio terminado

--- app/src/main/java/com/example/ejerciciofer/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciofer/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.ejerciciofer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.button1)
+        val switch1 = findViewById<SwitchCompat>(R.id.switch1)
+        val txedit = findViewById<EditText>(R.id.editText)
+
+        switch1.setOnClickListener {
+            btn1.isEnabled = switch1.isChecked == true
+
+        }
+
+        btn1.setOnClickListener {
+            if(txedit.text.isNotEmpty())
+            {
+                Toast.makeText(this, "Hola ${txedit.text}!", Toast.LENGTH_SHORT).show()
+            }else Toast.makeText(this, "Escribe tu nombre", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
